95b9aedccc3445f8778954171c6c5af5d1f9abcb
Omar Chunza <odchunzag.une@gmail.com>
2023-09-27 18:03:04 -0500

    Actualización modulo Isearch a laravel 10.

--- Http/Livewire/Search.php
+++ Http/Livewire/Search.php
@@ -2,59 +2,62 @@
 
 namespace Modules\Isearch\Http\Livewire;
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
-use Modules\Icommerce\Repositories\ProductRepository;
-use Modules\Iblog\Repositories\PostRepository;
-use Modules\Isearch\Transformers\SearchItemTransformer;
 
 class Search extends Component
 {
-  public $view;
-  public $search;
-  public $defaultView;
-  public $params;
-  public $results;
-  public $showModal;
-  public $icon;
-  public $placeholder;
-  public $title;
-  public $minSearchChars;
-  public $goToRouteAlias;
+    public $view;
+
+    public $search;
+
+    public $defaultView;
+
+    public $params;
+
+    public $results;
+
+    public $showModal;
+
+    public $icon;
+
+    public $placeholder;
+
+    public $title;
+
+    public $minSearchChars;
+
+    public $goToRouteAlias;
   public $labelButton;
   public $withLabelButton;
 
-  protected $queryString = [
-    'search' => ['except' => ''],
-  ];
+    protected $queryString = [
+        'search' => ['except' => ''],
+    ];
 
-
-  public function mount($layout = 'search-layout-1', $showModal = false, $icon = 'fa fa-search', $placeholder = null,
+    public function mount($layout = 'search-layout-1', $showModal = false, $icon = 'fa fa-search', $placeholder = null,
                         $title = '', $params = [], $minSearchChars = null, $goToRouteAlias = null, $labelButton = null,
                         $withLabelButton = false)
-  {
-    $this->defaultView = 'isearch::frontend.livewire.search.layouts.search-layout-1.index';
-    $this->view = isset($layout) ? 'isearch::frontend.livewire.search.layouts.' . $layout . '.index' : $this->defaultView;
-    $this->results = [];
-    $this->showModal = isset($showModal) ? $showModal : false;
-    $this->icon = isset($icon) ? $icon : 'fa-search';
-    $this->placeholder = $placeholder ?? trans('isearch::common.form.search_here');
-    $this->title = $title;
+    {
+        $this->defaultView = 'isearch::frontend.livewire.search.layouts.search-layout-1.index';
+        $this->view = isset($layout) ? 'isearch::frontend.livewire.search.layouts.'.$layout.'.index' : $this->defaultView;
+        $this->results = [];
+        $this->showModal = isset($showModal) ? $showModal : false;
+        $this->icon = isset($icon) ? $icon : 'fa-search';
+        $this->placeholder = $placeholder ?? trans('isearch::common.form.search_here');
+        $this->title = $title;
 
-    $this->params = $params;
-    $this->minSearchChars = $minSearchChars ?? setting('isearch::minSearchChars', null, "3");
-    $this->goToRouteAlias = $goToRouteAlias ?? config('asgard.isearch.config.route', 'isearch.search');
-    $repos = json_decode(setting('isearch::repoSearch'));
-    $this->params['filter']['repositories'] = $this->params['filter']['repositories'] ?? $repos;
+        $this->params = $params;
+        $this->minSearchChars = $minSearchChars ?? setting('isearch::minSearchChars', null, '3');
+        $this->goToRouteAlias = $goToRouteAlias ?? config('asgard.isearch.config.route', 'isearch.search');
+        $repos = json_decode(setting('isearch::repoSearch'));
+        $this->params['filter']['repositories'] = $this->params['filter']['repositories'] ?? $repos;
     $this->labelButton = $labelButton;
     $this->withLabelButton = $withLabelButton;
 
-  }
+    }
 
-  public function render()
-  {
-    return view($this->view);
-  }
+    public function render()
+    {
+        return view($this->view);
+    }
 }
